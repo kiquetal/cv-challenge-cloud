077c16f16bf23143ecad9dffcf7deb30a950c275
Enrique M. Talavera <kiquetal@gmail.com>
2021-05-30 19:37:09 -0400

    removing lines

--- backend/counter/index.ts
+++ backend/counter/index.ts
@@ -2,20 +2,10 @@ import { AzureFunction, Context, HttpRequest } from "@azure/functions"
 
 import { CosmosClient }  from  '@azure/cosmos';
 
-interface ContextExtended extends Context {
-    bindings: {
-        visitorDocument: any
-    }
-
-    Response: {
-        status?: number
-        body: string
-    }
-}
 
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
-    context.log('HTTP trigger function processed a request.');
+    context.log('HTTP trigger function processed a request.-');
     const name = (req.query.name || (req.body && req.body.name));
 
 
@@ -38,25 +28,5 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
 };
 
 
-export interface HttpResponseTest {
-  status: number;
-  body: string;
-}
-
-export interface HttpRequestTest {
-  body: {
-    name?: string,
-  };
-  query: {
-    name?: string,
-  };
-}
-
-
-
-export interface ContextTest {
-  log: (...message: any[]) => void;
-  done: (err: Error | null, res: HttpResponseTest) => void;
-}
 
 export default httpTrigger;
